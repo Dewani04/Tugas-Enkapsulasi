26dd205a7edfb124c04f138b596aa18767724bc0
Dewani04 <96469642+Dewani04@users.noreply.github.com>
2022-11-18 09:42:58 +0700

    Add files via upload

---
+++ Persegi.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author wani
+ */
+public class Persegi {
+    
+    private int Panjang;
+    private int Lebar;
+  
+    public int getLebar(){
+        return Lebar;
+    }
+    public void setLebar (int Lebar){
+        this.Lebar = Lebar;
+    }     
+     public int getPanjang() {
+         return Panjang;
+     }
+     public void setPanjang( int Panjang) {
+         this.Panjang = Panjang;
+    }
+     
+   
+     public Persegi (){
+         int p = 2;
+         int l = 4
+     }
+     
+     public int Luas (int P, int L) {
+         return P* 1;
+     }    
+    public int getLuas() {
+        return Luas (Panjang, Lebar);
+    }
+}
+    }     
+     }
+         
+     }
+     
+
+
